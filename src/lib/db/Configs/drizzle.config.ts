9fb0ac1b56ab7ba95592a0a4bc8ffbe533e90347
Qasrawi03Ghassan <gqasrawig@gmail.com>
2026-07-07 17:48:41 +0300

    Changed 'out' to 'migrations'

--- src/lib/db/Configs/drizzle.config.ts
+++ src/lib/db/Configs/drizzle.config.ts
@@ -3,7 +3,7 @@ import {readConfig} from './dbConfig';
 
 export default defineConfig({
   schema: "src/lib/db/schemas/schema.ts",
-  out: "src/lib/db/out",
+  out: "src/lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: readConfig().dbUrl,
